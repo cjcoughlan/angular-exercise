ed6fe3b9b5eb3354fbcac0f178fec8b84f6f3e8f
cjcoughlan <cale_coughlan@yahoo.com>
2015-08-05 07:48:21 -0500

    Initial Version

---
+++ AngularJSAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using AngularJSAuthentication.Models;
+using Microsoft.AspNet.Identity;
+
+namespace AngularJSAuthentication.Controllers
+{
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+        private AuthRepository _repo = null;
+
+        public AccountController()
+        {
+            _repo = new AuthRepository();
+        }
+
+        //POST api/Account/Register
+        [AllowAnonymous]
+        [Route("Register")]
+        public async Task<IHttpActionResult> Register(UserModel userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            IdentityResult result = await _repo.RegisterUser(userModel);
+
+            IHttpActionResult errorResult = GetErrorResult(result);
+
+            return errorResult ?? Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repo.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("",error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    //No Modelstate errors are available to send so just return bad request
+                    return BadRequest();
+                }
+                return BadRequest(ModelState);
+            }
+
+            return null;
+            
+            throw new NotImplementedException();
+        }
+    }
+}
